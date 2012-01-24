a58f98472a1be0c163d32c28c1c1c1a5938cb676
winzou <alexandre.bacco@gmail.com>
2012-01-24 18:54:04 +0100

    [2] Génération du bundle SdzBlogBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Sdz\BlogBundle\SdzBlogBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
