62048779b2f4fc03c3821a1bd443a2ae08e2ffe1
winzou <alexandre.bacco@gmail.com>
2012-02-19 17:09:51 +0100

    [9.1] Modification de la méthode supprimerAction()

--- src/Sdz/BlogBundle/Controller/BlogController.php
+++ src/Sdz/BlogBundle/Controller/BlogController.php
@@ -23,33 +23,33 @@ class BlogController extends Controller
         // Mais pour l'instant, on ne fait qu'appeler le template.
         return $this->render('SdzBlogBundle:Blog:index.html.twig');
     }
-    
-    
+
+
     public function voirAction($id)
     {
         // Ici, on récupérera l'article correspondant à l'id $id.
-        
+
         return $this->render('SdzBlogBundle:Blog:voir.html.twig');
     }
-    
+
     public function ajouterAction()
     {
         // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
-        
+
         if( $this->get('request')->getMethod() == 'POST' )
         {
             // Ici, on s'occupera de la création et de la gestion du formulaire.
-            
-            $this->get('session')->setFlash('notice', 'Article bien enregistré');
-        
+
+            $this->get('session')->setFlash('info', 'Article bien enregistré');
+
             // Puis on redirige vers la page de visualisation de cet article.
             return $this->redirect( $this->generateUrl('sdzblog_voir', array('id' => 5)) );
         }
-        
+
         // Si on n'est pas en POST, alors on affiche le formulaire.
         return $this->render('SdzBlogBundle:Blog:ajouter.html.twig');
     }
-    
+
     public function modifierAction($id)
     {
         // Ici, on récupérera l'article correspondant à l'$id.
@@ -63,8 +63,16 @@ class BlogController extends Controller
     {
         // Ici, on récupérera l'article correspondant à l'$id.
 
-        // Ici, on gérera la suppression de l'article en question.
+        if( $this->get('request')->getMethod() == 'POST' )
+        {
+            // Si la requête est en POST, on supprimera l'article.
+            $this->get('session')->setFlash('info', 'Article bien supprimé');
 
+            // Puis on redirige vers l'accueil.
+            return $this->redirect( $this->generateUrl('sdzblog_accueil') );
+        }
+
+        // Si la requête est en GET, on affiche une page de confirmation avant de supprimer.
         return $this->render('SdzBlogBundle:Blog:supprimer.html.twig');
     }
 }
